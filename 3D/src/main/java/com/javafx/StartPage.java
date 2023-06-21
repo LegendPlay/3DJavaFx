1192508132db752ad9687b9c097a9c4c5a4e45f7
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-21 19:28:02 +0200

    add start page as entry point of app

--- 3D/src/main/java/com/javafx/StartPage.java
+++ 3D/src/main/java/com/javafx/StartPage.java
@@ -1,7 +1,36 @@
 package com.javafx;
 
-public class StartPage {
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartPage extends Application {
+    private static Scene scene;
+
     public static void main(String[] args) {
-        FlightSimulatorGame.launch(FlightSimulatorGame.class, args);
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("startMenuController"), 1440, 800);
+
+        stage.setTitle("Flight Simulator");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+    
+    public void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/" + fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 }
