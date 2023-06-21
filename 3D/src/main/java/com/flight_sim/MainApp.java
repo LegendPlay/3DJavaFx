5fece1693fb4da15326a1425350ea5e876ecc75e
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-21 15:34:37 +0200

    move terrain generation and camera to other file

--- 3D/src/main/java/com/flight_sim/MainApp.java
+++ 3D/src/main/java/com/flight_sim/MainApp.java
@@ -1,98 +1,34 @@
 package com.flight_sim;
 
 import javafx.animation.AnimationTimer;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.scene.Camera;
 import javafx.scene.Group;
-import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.floor;
-
 public class MainApp extends Application {
     // constants
-    public static final int WIDTH = 1440;
-    public static final int HEIGHT = 800;
+    private static final int STAGE_WIDTH = 1440;
+    private static final int STAGE_HEIGHT = 800;
 
     @Override
     public void start(Stage stage) throws IOException {
-        // change to set number of boxes per row
-        int boxesperrow = 255;
-        int boxes = boxesperrow * boxesperrow;
-
-        // creating new array for the landscape to be stored
-        Box[] cubes;
-        cubes = new Box[boxes];
-
-        // creating the group that is later added to the scene
+        // creating the group that is added to the scene
         Group group = new Group();
+        Scene scene = new Scene(group, STAGE_WIDTH, STAGE_HEIGHT, true);
 
-        // materials
-        final PhongMaterial grass = new PhongMaterial();
-        grass.setSpecularColor(Color.LIGHTGREEN);
-        grass.setDiffuseColor(Color.GREEN);
+        // terrain
+        TerrainGeneration terrainGeneration = new TerrainGeneration();
+        terrainGeneration.createTerrain(group, TerrainGeneration.BOXES_PER_ROW);
 
-        // creating the landscape
-        for (int i = 0; i < boxes; i++) {
-
-            cubes[i] = new Box(50, ceil(Math.random() * 150), 50);
-            cubes[i].translateXProperty().set(i % Math.sqrt((double) boxes) * 50);
-            cubes[i].translateZProperty().set(floor((double) i / Math.sqrt((double) boxes)) * 50);
-            cubes[i].setMaterial(grass);
-            group.getChildren().add(cubes[i]);
-
-        }
-
-        Camera camera = new PerspectiveCamera(true);
-
-        // creating the scene
-        Scene scene = new Scene(group, WIDTH, HEIGHT, true);
-        scene.setFill(Color.LIGHTBLUE);
-        scene.setCamera(camera);
-
-        // setting up the camera
-        camera.translateZProperty().set(-500);
-        camera.setNearClip(20);
-        camera.setFarClip(2000);
-        camera.translateXProperty().set(floor((double) boxesperrow * 50 / 2));
-        camera.translateYProperty().set(-250);
-        camera.translateZProperty().set(50);
+        CameraHandler camera = new CameraHandler();
+        camera.setupCamera(scene, TerrainGeneration.BOXES_PER_ROW);
 
         // manages the user inputs
-        stage.addEventHandler(KeyEvent.KEY_PRESSED, keyEvent -> {
-            switch (keyEvent.getCode()) {
-                case A:
-                    camera.translateXProperty().set(camera.getTranslateX() - 10);
-                    break;
-                case D:
-                    camera.translateXProperty().set(camera.getTranslateX() + 10);
-                    break;
-                case W:
-                    camera.translateYProperty().set(camera.getTranslateY() - 5);
-                    break;
-                case S:
-                    camera.translateYProperty().set(camera.getTranslateY() + 5);
-                    break;
-                case LEFT:
-                    camera.rotateProperty().set(camera.getRotate() - 2);
-                    break;
-                case RIGHT:
-                    camera.rotateProperty().set(camera.getRotate() + 2);
-                    break;
-                default:
-                    break;
-            }
-        });
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, camera::handleKeyPress);
 
         // makes stage displayable
         stage.setTitle("Flight Simulator");
@@ -104,10 +40,7 @@ public class MainApp extends Application {
         AnimationTimer animationTimer = new AnimationTimer() {
             @Override
             public void handle(long arg0) {
-                // smooth movement
-                TranslateTransition translateTransition = new TranslateTransition(Duration.millis(100), camera);
-                camera.translateZProperty().set(camera.getTranslateZ() + 5);
-                translateTransition.play();
+                camera.handleAnimationTick();
             }
         };
         animationTimer.start();
