f62bb2519eb61c00014307b8c2b08ec331bc3eb7
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-23 22:14:01 +0200

    add camera and terrain to game

--- 3D/src/main/java/com/javafx/FlightSimulatorGame.java
+++ 3D/src/main/java/com/javafx/FlightSimulatorGame.java
@@ -1,10 +1,12 @@
 package com.javafx;
 
 import javafx.animation.AnimationTimer;
+import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.MeshView;
 import javafx.stage.Stage;
 
 public class FlightSimulatorGame {
@@ -16,20 +18,26 @@ public class FlightSimulatorGame {
         Group group = new Group();
         Scene scene = new Scene(group, STAGE_WIDTH, STAGE_HEIGHT, true);
 
-        CameraHandler camera = new CameraHandler();
-        camera.setupCamera(scene);
+        // camera
+        CameraHandler cameraHandler = new CameraHandler();
+        Camera camera = cameraHandler.setupCamera();
+
+        scene.setCamera(camera);
+        scene.setFill(Color.LIGHTBLUE);
 
         // terrain
-        Terrain terrain = new Terrain(group, 400, camera);
+        Terrain terrain = new Terrain();
+        MeshView mesh = terrain.generateTerrain(400, cameraHandler);
+        group.getChildren().add(mesh);
 
         // manage user input
-        scene.addEventHandler(KeyEvent.KEY_PRESSED, camera::handleKeyPress);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, cameraHandler::handleKeyPress);
 
         // main game ticks
         AnimationTimer animationTimer = new AnimationTimer() {
             @Override
             public void handle(long arg0) {
-                camera.handleAnimationTick();
+                cameraHandler.handleAnimationTick();
             }
         };
         animationTimer.start();
