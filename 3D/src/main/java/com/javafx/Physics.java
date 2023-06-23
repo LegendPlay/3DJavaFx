42704ce14d98d66beac1a616c21e0ccb826bf3ed
NMMN2 <125464473+NMMN2@users.noreply.github.com>
2023-06-23 16:37:05 +0100

    Renamed class

---
+++ 3D/src/main/java/com/javafx/Physics.java
@@ -0,0 +1,9 @@
+package com.javafx;
+
+public class Physics {
+    private static final double GRAVITATIONAL_ACCELERATION = 9.8067;
+    private static final double DRAG_COEFFICIENT_FLIGHT = 0.012;
+    private static final double WING_AREA = 525.0;
+    Physics() {}
+    
+}
