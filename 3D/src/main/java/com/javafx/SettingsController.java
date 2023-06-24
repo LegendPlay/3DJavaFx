3913411a93e20f8460b3315d4a7aed78c383c858
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-24 15:17:12 +0200

    ESC replicates go back button

--- 3D/src/main/java/com/javafx/SettingsController.java
+++ 3D/src/main/java/com/javafx/SettingsController.java
@@ -1,11 +1,20 @@
 package com.javafx;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 
-public class SettingsController {
+public class SettingsController implements Initializable{
     private boolean cameFromStartMenu;
+
+    @FXML
+    AnchorPane anchorPane;
 
     public SettingsController(boolean cameFromStartMenu) {
         this.cameFromStartMenu = cameFromStartMenu;
@@ -24,5 +33,18 @@ public class SettingsController {
             FlightSimulatorGame game = new FlightSimulatorGame();
             StartPage.setScene(game.startGame());
         }
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        anchorPane.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
+            if (e.getCode().equals(KeyCode.ESCAPE)) {
+                try {
+                    goBackScene();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }); 
     }
 }
