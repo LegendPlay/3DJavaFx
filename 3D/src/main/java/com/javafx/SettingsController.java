b1fef2ca726936a31d017e66b692848595478be1
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-24 15:05:13 +0200

    logic for go back button in settings menu

--- 3D/src/main/java/com/javafx/SettingsController.java
+++ 3D/src/main/java/com/javafx/SettingsController.java
@@ -5,13 +5,24 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class SettingsController {
+    private boolean cameFromStartMenu;
+
+    public SettingsController(boolean cameFromStartMenu) {
+        this.cameFromStartMenu = cameFromStartMenu;
+    }
+
     @FXML
-    private void goToStartMenu () throws IOException {
-        StartPage.setRoot("startMenu");
+    private void goToStartMenu() throws IOException {
+        StartPage.setScene("startMenu");
     }
 
     @FXML
     private void goBackScene() throws IOException {
-        
+        if (cameFromStartMenu) {
+            StartPage.setScene("startMenu");
+        } else {
+            FlightSimulatorGame game = new FlightSimulatorGame();
+            StartPage.setScene(game.startGame());
+        }
     }
 }
