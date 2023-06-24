350967848bffb9485cd4263d99fe4658e044b3ee
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-24 15:02:54 +0200

    add static methods to set Scenes

--- 3D/src/main/java/com/javafx/StartPage.java
+++ 3D/src/main/java/com/javafx/StartPage.java
@@ -10,6 +10,7 @@ import javafx.stage.Stage;
 
 public class StartPage extends Application {
     private static Scene scene;
+    private static Stage stage;
 
     public static void main(String[] args) {
         launch();
@@ -19,15 +20,33 @@ public class StartPage extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("startMenu"), 1440, 800);
 
+        StartPage.stage = stage;
+
         stage.setTitle("Flight Simulator");
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
     }
-    
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+
+    static void setScene(Scene scene) {
+        stage.setScene(scene);
     }
+
+    static void setScene(String fxml) throws IOException {
+        Scene scene = new Scene(loadFXML(fxml));
+        stage.setScene(scene);
+    }
+
+    static void setSettingsScene(String fxml, boolean cameFromStartMenu) throws IOException {
+        FXMLLoader loader = new FXMLLoader(StartPage.class.getResource("/fxml/" + fxml + ".fxml"));
+        loader.setController(new SettingsController(cameFromStartMenu));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+    }
+    
+    // static void setRoot(String fxml) throws IOException {
+    //     scene.setRoot(loadFXML(fxml));
+    // }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartPage.class.getResource("/fxml/" + fxml + ".fxml"));
