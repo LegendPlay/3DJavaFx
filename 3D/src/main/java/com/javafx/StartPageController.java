801d3513d62870e7a9071c779af3496f2671755f
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-06-30 22:29:06 +0200

    add go savedWorlds Button

--- 3D/src/main/java/com/javafx/StartPageController.java
+++ 3D/src/main/java/com/javafx/StartPageController.java
@@ -9,10 +9,15 @@ import javafx.scene.control.Button;
 public class StartPageController {
     private int randomSeed = new Random().nextInt(10000);
     @FXML
-    private Button startButton;
+    private Button newGameButton;
 
     @FXML
-    private void startGame() throws IOException {
+    private void goToSavedWorlds() {
+
+    }
+
+    @FXML
+    private void createNewWorld() throws IOException {
         FlightSimulatorGame game = new FlightSimulatorGame();
         StartPage.setScene(game.startGame(randomSeed));
         SettingsHandler.put("seed", String.valueOf(randomSeed));
