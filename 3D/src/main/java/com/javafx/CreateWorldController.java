99fbc7daf01db7bf4005f8f40251449f8c022197
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-07-02 18:19:50 +0200

    data from user gets saved in db

--- 3D/src/main/java/com/javafx/CreateWorldController.java
+++ 3D/src/main/java/com/javafx/CreateWorldController.java
@@ -4,10 +4,18 @@ import java.io.IOException;
 import java.util.Random;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class CreateWorldController {
-    private int randomSeed = new Random().nextInt(10000);
+    private final String pattern = "([1-9]\\d{0,3}?|10000)";
+
+    @FXML
+    private TextField seedTextField;
+
+    @FXML
+    private TextField nameTextField;
 
     @FXML
     private AnchorPane anchorPane;
@@ -19,15 +27,53 @@ public class CreateWorldController {
 
     @FXML
     private void createWorld() {
-        FlightSimulatorGame game = new FlightSimulatorGame();
-        StartPage.setScene(game.startGame(randomSeed));
+        boolean nameValid = false;
+        boolean seedValid = false;
 
-        // SettingsHandler.put("seed", String.valueOf(randomSeed)); // TODO seed
+        String worldName = nameTextField.getText();
+        String seedText = seedTextField.getText();
+        int userSeed = 0;
+
+        if (worldName.isBlank()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Name");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a valid name for this world");
+            alert.showAndWait();
+        } else {
+            nameValid = true;
+        }
+
+        if (seedText.matches(pattern)) {
+            userSeed = Integer.parseInt(seedText);
+            seedValid = true;
+        } else if (seedText.equals("")) {
+            userSeed = new Random().nextInt(10000);
+            seedValid = true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalid Seed");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a number between 1 and 10000 as a seed.");
+            alert.showAndWait();
+
+            seedTextField.clear();
+        }
+
+        if (nameValid && seedValid) {
+            SettingsHandler.createGame(userSeed, worldName);
+
+            GameData data = SettingsHandler.readGameData(SettingsHandler.getNewestWorldId());
+
+            FlightSimulatorGame game = new FlightSimulatorGame(data);
+            StartPage.setScene(game.startGame());
+        }
+
     }
 
     @FXML
     private void goToSettings() throws IOException {
-        StartPage.setSettingsScene("settingsMenu", "createWorldMenu");
+        StartPage.setSettingsScene("settingsMenu", "createWorldMenu", FlightSimulatorGame.world_id);
     }
 
     @FXML
