d4619174c8c6c1bb1462dd3a4b664806ab70dbdd
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-07-02 11:39:20 +0200

    alert dialog when close game request

--- 3D/src/main/java/com/javafx/StartPage.java
+++ 3D/src/main/java/com/javafx/StartPage.java
@@ -6,10 +6,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class StartPage extends Application {
-    private static Scene scene;
     private static Stage stage;
 
     public static void main(String[] args) {
@@ -22,12 +24,42 @@ public class StartPage extends Application {
 
         SettingsHandler.initializeDatabase();
 
-        scene = new Scene(loadFXML("startMenu"), 1440, 800);
+        Scene scene = new Scene(loadFXML("startMenu"), 1440, 800);
+
+        stage.setOnCloseRequest(this::onWindowClose);
 
         stage.setTitle("Flight Simulator");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+    }
+
+    private void onWindowClose(WindowEvent event) {
+        // show alert
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Save Game");
+        alert.setHeaderText("Do you want to save the game before closing?");
+        alert.setContentText("Choose your option.");
+
+        ButtonType saveButton = new ButtonType("Save");
+        ButtonType closeButton = new ButtonType("Close without saving");
+        ButtonType cancelButton = new ButtonType("Cancel");
+
+        alert.getButtonTypes().setAll(saveButton, closeButton, cancelButton);
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == saveButton) {
+                // save the game logic here
+                System.out.println("Game Saved!");
+            } else if (response == closeButton) {
+                // user chose to close the window without saving
+                event.consume();
+                stage.close();
+            } else {
+                // user chose to cancel the close request
+                event.consume();
+            }
+        });
     }
 
     public static void closeStage() {
@@ -43,16 +75,12 @@ public class StartPage extends Application {
         stage.setScene(scene);
     }
 
-    public static void setSettingsScene(String fxml, boolean cameFromStartMenu) throws IOException {
+    public static void setSettingsScene(String fxml, String cameFromMenu) throws IOException {
         FXMLLoader loader = new FXMLLoader(StartPage.class.getResource("/fxml/" + fxml + ".fxml"));
-        loader.setController(new SettingsController(cameFromStartMenu));
+        loader.setController(new SettingsController(cameFromMenu));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
     }
-
-    // static void setRoot(String fxml) throws IOException {
-    // scene.setRoot(loadFXML(fxml));
-    // }
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartPage.class.getResource("/fxml/" + fxml + ".fxml"));
